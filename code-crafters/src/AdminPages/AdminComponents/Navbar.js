b5e343cce7896b8c511abc86844d524ac0b80f4f
zbytes2227 <zbytes1@outlook.com>
2023-04-27 08:12:55 +0530

    FUll Stack 1.14

--- code-crafters/src/AdminPages/AdminComponents/Navbar.js
+++ code-crafters/src/AdminPages/AdminComponents/Navbar.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
 <header class="text-gray-600 body-font">
+<h6 className='text-center bg-gray-200'>BASE TRIAL VERSION 3.77 - UnderDev - ZBYTES 😁</h6>
   <div class="container mx-auto flex flex-wrap p-5 flex-col items-center">
     <nav class="flex flex-wrap items-center text-base md:m-auto">
       <a href='/admin/dashboard/' class="mr-5 hover:text-gray-900">Home</a>
--- code-crafters/src/App.js
+++ code-crafters/src/App.js
@@ -20,7 +20,7 @@ import AssignmentShow from './Pages/AssignmentShow';
 function App() {
   return (
     <>
-    <h6 className='text-center bg-gray-200'>BASE TRIAL VERSION - UnderDev</h6>
+    <h6 className='text-center bg-gray-200'>BASE TRIAL VERSION 1.11 - UnderDev - ZBYTES 😁</h6>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />

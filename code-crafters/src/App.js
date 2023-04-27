ba4a7d6b5b8ca04a34cc3f8cbf3a22d1ab4a5c1b
zbytes2227 <zbytes1@outlook.com>
2023-04-27 08:47:40 +0530

    FUll Stack 1.19 complete

--- code-crafters/src/App.js
+++ code-crafters/src/App.js
@@ -20,7 +20,9 @@ import AssignmentShow from './Pages/AssignmentShow';
 function App() {
   return (
     <>
-    <h6 className='text-center bg-gray-200'>BASE TRIAL VERSION 1.11 - UnderDev - ZBYTES 😁</h6>
+    {/* <h6 className='text-center bg-gray-200'>BASE TRIAL VERSION 1.11 - UnderDev - ZBYTES 😁</h6> */}
+    <h6 className="text-center bg-gray-200 sticky top-0">BASE TRIAL VERSION 1.11 - UnderDev - ZBYTES 😁</h6>
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
